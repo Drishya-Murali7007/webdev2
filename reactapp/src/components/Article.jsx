833c204ae1b134bd9843c5e0dcc6b9a3d80fb913
Drishya-Murali7007 <muralidrishya26@gmail.com>
2026-04-15 15:45:59 +0530

    addded css

--- reactapp/src/components/Article.jsx
+++ reactapp/src/components/Article.jsx
@@ -1,5 +1,9 @@
 import React ,{useState}from 'react'
 import artstyle from '../css/Article.module.css'
+// import styled from 'styled-components'
+import styled from '@emotion/styled'
+import {css} from '@emotion/react'
+
 
 function Article() {
     const [count , setCount ]=useState(0)
@@ -21,8 +25,25 @@ function Article() {
       <h2> {count}</h2>
       <button className={artstyle.btn} onClick={handleIncrement}>Increment</button>
       <button className={artstyle.btn} onClick={handleDecrement}>Decrement</button>
+
+      <Button> Click me  </Button>
+      <Button red> Dont click mi</Button>
+      <button css ={{backgroundColor:'olive', color:'white', padding:'10px 20px', border:'none', borderRadius:'5px', cursor:'pointer'}}> i DOnt exist</button>
+      <button className='bg-pink-500 text-white border-4'> absurdism</button>
+
     </div>
   )
 }
 
 export default Article
+
+const Button = styled.button`
+background-color: ${props => props.red ? 'red' : 'pink '} ;     
+color: blue ;
+padding: 10px 20px;
+border: none;
+border-radius: 5px;
+cursor: pointer;
+ ;
+    `
+
